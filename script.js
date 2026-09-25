3147aa377fd58d41e9d1e234ec439c1579418895
allprojectwsen <wisnumahindra005@gmail.com>
2026-09-25 16:42:09 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbwCLZrt29EQ_CTT11r9OC0EKha4wajjnTEYR9wzTicyGi93zf-xRtDnR770q1tqw9C9/exec";
+
+// File ini digunakan sebagai konfigurasi API utama.
+// Fungsi absensi dan admin saat ini berada di halaman masing-masing.
